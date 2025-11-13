d09fe2a7e4bda98bca3f4119d825a1486dd567bf
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-11-13 20:25:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+  let n = parseInt(document.getElementById("rope-lengths").value);
+  let arr = [];
+  while (n > 0) {
+    arr.push(parseInt(prompt("Enter the length of the rope")));
+    n--;
+  }
+  let sum = 0;
+  while (arr.length > 1) {
+    arr.sort((a, b) => a - b);
+    let a = arr.shift();
+    let b = arr.shift();
+    let cost = a + b;
+    sum += cost;
+    arr.push(cost);
+  }
+  document.getElementById("result").innerText = "Minimum cost to connect ropes: " + sum;
+}
+
